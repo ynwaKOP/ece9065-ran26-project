2d5171497fcf818a6f45102fef288e4a6b0e3b88
KOPRA <kopra@KOPRAs-MacBook-Pro.local>
2020-12-02 17:17:22 -0500

    add css or the list component

--- src/app/lists/show-list/show-list.component.ts
+++ src/app/lists/show-list/show-list.component.ts
@@ -2,9 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-show-list',
-    templateUrl: './show-list.component.html'
+    templateUrl: './show-list.component.html',
+    styleUrls: ['./show-list.component.css']
 
 })
 export class ShowListComponent {
+    /*lists = [
+        {name: "my list 111", description:'optional 123123', pairs:[]},
+        {name: "my list 222", description:'', pairs:[]},
+        {name: "my list 333", description:'optional 33633', pairs:[]},
+        {name: "my list 444", description:'', pairs:[]}
+    ]*/
 
+    lists = [];
 }
