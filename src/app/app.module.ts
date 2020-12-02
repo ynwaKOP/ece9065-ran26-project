007643d47d509f16128f0b3ebb45c04a7894ccc1
KOPRA <kopra@KOPRAs-MacBook-Pro.local>
2020-12-02 13:18:51 -0500

    add course lists componets with placeholders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ListCreateComponent } from './lists/list-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListCreateComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/lists/list-create.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    selector: 'app-list-create',
+    templateUrl: './list-create.component.html'
+})
+export class ListCreateComponent {
+
+}
