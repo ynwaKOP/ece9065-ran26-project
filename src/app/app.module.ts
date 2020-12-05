6c8594d917d59de55a7a8775d24558bcb79b1273
KOPRA <kopra@KOPRAs-MacBook-Pro.local>
2020-12-04 23:50:05 -0500

    table look search sub+code  result

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,14 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { HttpClientModule } from '@angular/common/http';
+import {CdkTableModule} from '@angular/cdk/table';
+import { MatTableModule } from '@angular/material/table';
+
 
 import { AppComponent } from './app.component';
 import { ListCreateComponent } from './lists/list-create.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 import { ShowListComponent } from './lists/show-list/show-list.component';
 import { VisitorPageComponent } from './visitor/visitor.component'
@@ -34,6 +38,9 @@ import { VisitorPageComponent } from './visitor/visitor.component'
     MatButtonModule,
     MatToolbarModule,
     MatExpansionModule,
+    MatTableModule,
+    CdkTableModule,
+    BrowserAnimationsModule,
     HttpClientModule
   ],
   providers: [],
--- src/app/visitor/visitor.component.ts
+++ src/app/visitor/visitor.component.ts
@@ -3,11 +3,19 @@ import { CourseService } from '../course.service';
 import { Course } from '../course.model';
 import { NgForm } from '@angular/forms';
 import { List } from '../lists/list.model'
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
     selector: 'app-visitor-page',
     templateUrl: './visitor.component.html',
-    styleUrls: ['./visitor.component.css']
+    styleUrls: ['./visitor.component.css'],
+    animations: [
+        trigger('detailExpand', [
+          state('collapsed', style({height: '0px', minHeight: '0', display: 'none'})),
+          state('expanded', style({height: '*'})),
+          transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+        ]),
+      ],
 })
 
 export class VisitorPageComponent implements OnInit {
@@ -15,7 +23,12 @@ export class VisitorPageComponent implements OnInit {
     
     pubLists: List[];
     courses: Course[];
-    
+
+    cols = ['subject', 'code', 'name', 'section', 
+        'component'];
+
+    expandedElement: Course;
+
     constructor(private courseService: CourseService) { }
 
     
@@ -23,7 +36,7 @@ export class VisitorPageComponent implements OnInit {
     }
 
 
-    searchCombo(form: NgForm) {
+    searchCombo(form: NgForm): Course[] {
         if (form.invalid) {
             return;
         }
@@ -33,6 +46,5 @@ export class VisitorPageComponent implements OnInit {
     }
     
 }
-
 
 
